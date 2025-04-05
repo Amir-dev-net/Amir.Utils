8274918bbdcc93437b40b502b3bc3e2175c2798e
amirhamza <amirhamza@vsm.com>
2025-04-05 14:35:26 +0530

    Modified program.cs

--- Amir.Utils.Demo/Program.cs
+++ Amir.Utils.Demo/Program.cs
@@ -7,10 +7,39 @@ namespace Amir.Utils.Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(StringHelper.ToTitleCase("hello from amir!"));
+            Console.WriteLine("🔤 String Helper Examples:\n");
 
-            var readable = DateTimeHelper.ToHumanReadable(DateTime.UtcNow.AddMinutes(-45));
-            Console.WriteLine(readable);
+            string input = "hello from amir!";
+            Console.WriteLine($"Original: {input}");
+            Console.WriteLine($"ToTitleCase: {StringHelper.ToTitleCase(input)}");
+
+            string slugInput = "Hello, World! This is a test.";
+            Console.WriteLine($"\nOriginal: {slugInput}");
+            Console.WriteLine($"\nSlugify: {StringHelper.Slugify(slugInput)}");
+
+            string specialChars = "Hi@# Amir!";
+            Console.WriteLine($"\nOriginal: {specialChars}");
+            Console.WriteLine($"\nRemoved Special Characters: {StringHelper.RemoveSpecialCharacters(specialChars)}");
+
+
+            Console.WriteLine("\n--------------------------------------------:\n");
+            Console.WriteLine("\n📅 DateTime Helper Examples:\n");
+
+            DateTime past = DateTime.UtcNow.AddMinutes(-45);
+            Console.WriteLine($"Original: {past}");
+            Console.WriteLine($"ToHumanReadable (45 mins ago): {DateTimeHelper.ToHumanReadable(past)}");
+
+            DateTime today = DateTime.Today;
+            Console.WriteLine($"\nOriginal: {today}");
+            Console.WriteLine($"IsWeekend (Today): {DateTimeHelper.IsWeekend(today)}");
+
+            DateTime startDate = DateTime.Today.AddDays(-10);
+            Console.WriteLine($"\nOriginal: {startDate}");
+            DateTime endDate = DateTime.Today;
+            Console.WriteLine($"\nOriginal: {endDate}");
+            Console.WriteLine($"BusinessDaysBetween (-10 days to today): {DateTimeHelper.BusinessDaysBetween(startDate, endDate)}");
+
+            Console.WriteLine("\n✅ Done.");
         }
     }
 }
